d1a6ea4b6e200dde91e61da91675d5e3bb839cc0
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-01-28 20:51:38 -0300

    Primeira folha de JavaScript criada, produtos inclusos

---
+++ scripts/dataproduct.js
@@ -0,0 +1,32 @@
+const listaDeAcessorios = [
+    {
+        nome: 'Meia Bebê Branca',
+        descricao: 'Com formato anatômico e materiais de qualidade, essa é uma ideia diferente e original que vai levar um toque lúdico à rotina de pais e filhos, já que que as meias estão disponíveis para crianças e adultos.',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/147094/meia-bebe-trocape-nuvem-e-ursinho-azul-marinho-323065.jpg'
+    },
+    {
+        nome: 'Calça Malha Azul',
+        descricao: 'Confeccionada em tecido 100% algodão com pézinho e elástico na cintura, a peça é uma sugestão de presente super prática para completar o enxoval de bebê.',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/149014/calca-malha-tricotada-azul-marinho-323684.jpg'
+    },
+    {
+        nome: 'Kit Berço Amiguinhas',
+        descricao: 'Com o Kit Berço Amiguinhas Safári Rosa ficou mais fácil escolher o enxoval perfeito para o quarto de bebê! Os protetores de berço são os diferenciais desse conjunto: quatro almofadas amiguinhas bordadas.',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/98853/kit-berco-amiguinhas-safari-rosa-233524.jpg'
+    },
+    {
+        nome: 'Meia Bebê Azul Marinho',
+        descricao: 'Com formato anatômico e materiais de qualidade, essa é uma ideia diferente e original que vai levar um toque lúdico à rotina de pais e filhos, já que que as meias estão disponíveis para crianças e adultos.',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/147106/meia-bebe-trocape-lisa-azul-marinho-323131.jpg'
+    },
+    {
+        nome: 'Kit Berço Trança',
+        descricao: 'Carinho em cada detalhe! Essa é a proposta do Kit Berço Trança Amor da Mamãe Rosa, um enxoval de bebê para transformar o ambiente num sonho cor-de-rosa!',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/150702/kit-berco-tranca-coracao-baby-rosa-328360.jpg'
+    },
+    {
+        nome: 'Calça Malha Cinza',
+        descricao: 'Confeccionada em tecido 100% algodão com pézinho e elástico na cintura, a peça é uma sugestão de presente super prática para completar o enxoval de bebê.',
+        imagem: 'https://dmhxz00kguanp.cloudfront.net/fotos/149016/calca-malha-tricotada-cinza-323687.jpg'
+    },
+]
